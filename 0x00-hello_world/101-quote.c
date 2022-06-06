d312be7524eaf7e0c858c6490af225f8eff9d24f
Teminikan <Temskidfaith@gmail.com>
2022-06-06 09:57:39 -0700

    task 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,11 @@
 
 /**
  * main - the entry point
- *
  * Return: outputs 1
- *
  */
 
 int main(void)
 {
 		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 		return (1);
 }
