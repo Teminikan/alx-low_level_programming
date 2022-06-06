ee0b8bf2883d793744f00d614cd099ef512b1c3b
Teminikan <Temskidfaith@gmail.com>
2022-06-06 09:28:27 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - The entry point of the program
+ *
+ *Return: Outputs the result of the function type - 0
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
