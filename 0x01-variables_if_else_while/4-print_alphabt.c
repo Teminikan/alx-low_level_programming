06d3b7611ee61b300053ce9ace95292c65c102cf
Teminikan <Temskidfaith@gmail.com>
2022-06-06 21:44:05 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ * Return: outps program result type, int
+ */
+int main(void)
+{
+	char start;
+
+	for (start = 'a'; start <= 'z'; start++)
+	{
+		if (start == 'q' || start == 'e')
+		{
+			continue;
+		}
+		putchar(start)
+	}
+	putchar('\n');
+	return (0);
+}
