51f6d54870438dce96b8edfb32a7b72a7b516946
Teminikan <Temskidfaith@gmail.com>
2022-06-08 14:33:53 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; start++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
