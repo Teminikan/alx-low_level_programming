8f2191e3ad93ff6e8fc282e33d2afa59da13f783
Teminikan <Temskidfaith@gmail.com>
2022-06-09 12:20:30 -0700

    task 1

---
+++ 1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * _isdigit - a functon 
+ * @c: input
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		return (0);
+}
