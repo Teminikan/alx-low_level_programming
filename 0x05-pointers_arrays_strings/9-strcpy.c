3844b5c4143b9bfa9819f4d549a09d9442e1fd7a
Teminikan <Temskidfaith@gmail.com>
2022-06-14 06:24:07 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,8 +4,7 @@
  * _strcpy - Copies a string pointed to by @src, including the
  * terminating null byte, to a buffer pointed to by @dest
  * @dest: A buffer to copy the string to
- * @src: A source string to copy
- * 
+ * @src: A source string to copy 
  * Return: A pointer
  */
 char *_strcpy(char *dest, char *src)
@@ -19,4 +18,4 @@ char *_strcpy(char *dest, char *src)
 	dest[index] = '\0';
 
 	return (dest);
-}	
+}
