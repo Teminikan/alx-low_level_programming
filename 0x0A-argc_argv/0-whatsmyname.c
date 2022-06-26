1907195d5c04cc8eda219942b84de8bf5c1a5feb
Teminikan <Temskidfaith@gmail.com>
2022-06-25 18:43:01 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,6 @@
  * main - Prints the number of arguments passed to it.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- *
  * Return: Always 0.
  */
 int main(int argc, char __attribute__((__unused__)) *argv[])
